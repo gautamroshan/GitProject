fba0bda6b3587ec1ebb4b5ad6ce03de3ef466514
gautamroshan <roshan.gautam@mavs.uta.edu>
2021-09-20 18:43:27 -0500

    divided code

--- hello.java
+++ hello.java
@@ -4,4 +4,10 @@ public class hello
 	 public static void main(String[] args) {
 		System.out.println("Hello everyone");
 	}
+	
+	public static void Roshan_part(){
+		System.out.println("Hello from Roshan");
+	}
+	
+	//create void method Kushal_part()
 }
